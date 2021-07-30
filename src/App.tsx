528f8c43bd629d1d016e2ffe584280ee410a8def
martinageradams <w3cfed@gmail.com>
2021-07-31 03:07:21 +0800

    feat(List): Add List FC Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styles from "./App.module.css";
+import logo from "./assets/images/logo.svg"
+import robot from "./mock/robots.json";
+import Robot from "./components/Robot/Robot";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.app}>
+      <div className={styles.appHeader}>
+        <img src={logo} className={styles.appLogo} alt="logo" />
+        <h1>机器人在线商城</h1>
+      </div>
+      <div className={styles.robotList}>
+        {robot.map((r) => (
+          <Robot id={r.id} name={r.name} email={r.email} />
+        ))}
+      </div>
     </div>
   );
 }
